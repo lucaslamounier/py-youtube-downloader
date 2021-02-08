6ad91424c73184b7feeb6f6610493cb2deeead57
lucaslamounier <lucasls.oas@gmail.com>
2021-02-07 23:08:49 -0300

    atualizando

--- downloader.py
+++ downloader.py
@@ -1,5 +1,6 @@
 import os
 import ffmpy
+import re
 from datetime import datetime
 from pytube import YouTube
 from pytube import Playlist
@@ -7,36 +8,38 @@ from pytube import Playlist
 DOWNLOAD_DIR = 'D:\\Videoclips\\'
 
 PLAYLISTS_URL = [
+    'https://www.youtube.com/watch?v=kkg9fmrZ_Lk&list=PLwnxR295e2u6Dn-30Fi0H-8WTBUa0Tx8B'
     #'https://www.youtube.com/watch?v=VpJKsoMWnZM&list=PLipAo7Mf9fxsjTq_lX6XD6dNdcHXfVK1H', # funk 2021
-    'https://www.youtube.com/watch?v=8SbUC-UaAxE&list=PL_Q15fKxrBb7wo5SRxHbhk6ZL6zMYh5tH', # Top rock todos os tempos
     #'https://www.youtube.com/watch?v=gUpGTRR4Tt4&list=PLB8HqqmpyIBcAHYt_w15AprSLViIGrToO', # reggae brasileiro
-    'https://www.youtube.com/watch?v=4b9_5xzAuSU&list=RDGMEMHDXYb1_DDSgDsobPsOFxpA', # Mix hip hop
-    'https://www.youtube.com/watch?v=YrQLmElRT-E&list=PL7V1hXWh2rMr4pz6lCkzHMHMLU3-BfQ2S' # Melhores tribo
+    #'https://www.youtube.com/watch?v=4b9_5xzAuSU&list=RDGMEMHDXYb1_DDSgDsobPsOFxpA', # Mix hip hop
+    #'https://www.youtube.com/watch?v=YrQLmElRT-E&list=PL7V1hXWh2rMr4pz6lCkzHMHMLU3-BfQ2S' # Melhores tribo
 ]
 
 ''' Realiza o download de uma determinada playlist '''
 def download_playlist(playlist_url):
     start_time = datetime.now()
     pl = Playlist(playlist_url)
+
+    if len(pl) == 0:
+        print('A playlist {} não contém videos'.format(playlist_url))
+        return
+
     print('Baixando playlist: ' + pl.title)
+    
     playlist_dir = get_or_create_playlist_dir(pl.title)
 
-    if not pl.title:
-        import pdb; pdb.set_trace()
-    
     for video in pl.videos:
         try:
-            if not exist_video_in_dir(video.title):
+            if not exist_video_in_dir(playlist_dir, video.title):
                 vid = video.streams.filter(
                     progressive=True, 
                     file_extension='mp4').order_by('resolution').desc().first()
 
+                print('Baixando video {}'.format(vid.title))
+
                 if vid:
                     vid.download(output_path=playlist_dir)
                 
-            else:
-                print('O video {} ja existe no diretorio {}'.format(video.title, playlist_dir))
-
         except Exception as error:
             print('Ocorreu um erro ao realizar download do video ', video)
             continue
@@ -47,7 +50,9 @@ def download_playlist(playlist_url):
 
 
 def get_or_create_playlist_dir(playlist_name):
-    directory = os.path.join(DOWNLOAD_DIR, playlist_name) 
+    pl_name = re.sub('[!,*)@#%(&$_?.^|]', '', playlist_name)
+    playlist = pl_name.replace(' ', '-')
+    directory = os.path.join(DOWNLOAD_DIR, playlist) 
 
     if not os.path.exists(directory):
         os.mkdir(directory)
@@ -57,14 +62,14 @@ def get_or_create_playlist_dir(playlist_name):
 
 
 ''' Verifica se o video já existe no diretorio '''
-def exist_video_in_dir(video_title):
-    videos = os.listdir(DOWNLOAD_DIR)
+def exist_video_in_dir(playlist_dir, video_title):
+    videos = os.listdir(playlist_dir)
     list_video_titles = []
 
     for video in videos:
         name =  os.path.splitext(video)[0]
         list_video_titles.append(name)
-    
+
     return True if video_title in list_video_titles else False
 
 
